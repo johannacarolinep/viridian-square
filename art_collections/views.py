5d055cf6616aa22eec26caefc7fc353c13a3233e
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-06-26 20:17:18 +0200

    feat: Add filtering by owner id to ArtCollectionList view

--- art_collections/views.py
+++ art_collections/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ArtCollectionSerializer
 from .models import ArtCollection
 
@@ -8,6 +9,13 @@ class ArtCollectionList(generics.ListCreateAPIView):
     serializer_class = ArtCollectionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = ArtCollection.objects.all()
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+
+    filterset_fields = [
+        'owner__id'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
