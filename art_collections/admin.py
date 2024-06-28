a68b1b46616a1ae4ca9ece45341b94c24b9c1c0e
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-06-28 17:07:24 +0200

    refactor: Clean and add docstring to art_collections admin.py file

--- art_collections/admin.py
+++ art_collections/admin.py
@@ -4,8 +4,24 @@ from art_collections.models import ArtCollection
 
 @admin.register(ArtCollection)
 class ArtCollectionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'owner', 'title', 'created_on', 'updated_on', 'get_artpieces')
+    """
+    Customizes the admin interface for the ArtCollection model,
+    specifying the fields to display in the list view and adding a custom
+    method to display related artpieces.
+    """
+    list_display = (
+        'id',
+        'owner',
+        'title',
+        'created_on',
+        'updated_on',
+        'get_artpieces'
+        )
 
     def get_artpieces(self, obj):
+        """
+        Returns a comma-separated string of artpieces
+        associated with the art collection.
+        """
         return ", ".join([str(ap) for ap in obj.collection_artpieces.all()])
     get_artpieces.short_description = 'Artpieces'
