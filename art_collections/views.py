9bbc1ece2e68c3dccd3c8a16b5f24a5571f5be49
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-06-28 11:41:52 +0200

    refactor: Clean up art_collections views, add docstrings

--- art_collections/views.py
+++ art_collections/views.py
@@ -11,12 +11,57 @@ from artpieces.models import Artpiece
 
 
 class ArtCollectionDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API view to retrieve, update, or delete an art collection.
+
+    - GET: Retrieve an art collection by its primary key.
+    - PUT: Update an art collection (owner only).
+    - DELETE: Delete an art collection (owner only).
+
+    Uses `ArtCollectionSerializer` for serialization.
+
+    Permissions:
+    - `IsOwnerOrReadOnly`: Owners can modify; others can only read.
+
+    Attributes:
+        serializer_class (ArtCollectionSerializer): Handles serialization.
+        permission_classes (list): Defines access permissions.
+        queryset (QuerySet): Base queryset for retrieving art collections.
+    """
     serializer_class = ArtCollectionSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = ArtCollection.objects.all()
 
 
 class ArtCollectionList(generics.ListCreateAPIView):
+    """
+    View to list all art collections or create a new art collection.
+
+    - GET: Returns a list of all art collections. Supports filtering by the
+    owner's ID.
+    - POST: Creates a new art collection (authenticated only). The
+    authenticated user is set as the owner.
+
+    Uses `ArtCollectionSerializer` for serialization.
+
+    Permissions:
+    - `IsAuthenticatedOrReadOnly`: Authenticated users can create; others can
+    only read.
+
+    Attributes:
+        serializer_class (ArtCollectionSerializer): Handles serialization.
+        permission_classes (list): Defines access permissions.
+        queryset (QuerySet): The base queryset for retrieving art collections.
+        filter_backends (list): List of filter backends used for filtering the
+        queryset.
+        filterset_fields (list): List of fields that can be used to filter the
+        queryset.
+
+    Methods:
+        perform_create(self, serializer):
+            Saves the new art collection instance with the authenticated user
+            set as the owner.
+    """
     serializer_class = ArtCollectionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = ArtCollection.objects.all()
@@ -27,27 +72,47 @@ class ArtCollectionList(generics.ListCreateAPIView):
         'owner__id'
     ]
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context.update({"request": self.request})
-        return context
-
     def perform_create(self, serializer):
+        """
+        Saves the new art collection instance with the authenticated user
+        set as the owner.
+        """
         serializer.save(owner=self.request.user)
 
 
 class ArtCollectionUpdateArtpieces(generics.GenericAPIView):
+    """
+    API view to bulk update the association of artpieces to an art collection.
+    Only the owner of the art collection can modify it. Only the owner's
+    artpieces can be added.
+
+    - POST: Adds/removes artpieces from a collection (collection owner only).
+
+    Uses `ArtCollectionSerializer` for serialization.
+
+    Methods:
+        post(self, request, *args, **kwargs):
+            Updates the artpieces in the collection based on the provided list
+            of artpiece IDs.
+    """
     queryset = ArtCollection.objects.all()
     serializer_class = ArtCollectionSerializer
     permission_classes = [IsOwner]
 
     def post(self, request, *args, **kwargs):
+        """
+        Updates the artpieces in the collection based on the provided list
+        of artpiece IDs. Adds new artpieces to the collection and removes
+        artpieces that are not in the provided list. Ensures art pieces to be
+        added to the collection belongs to the user.
+        """
         collection_id = kwargs.get('pk')
         new_artpiece_ids = request.data.get('artpiece_ids', [])
 
         try:
             art_collection = self.get_object()
-            existing_artpieces = Artpiece.objects.filter(art_collection=collection_id)
+            existing_artpieces = Artpiece.objects.filter(
+                art_collection=collection_id)
 
             # Remove artpieces that are no longer selected
             for artpiece in existing_artpieces:
@@ -60,11 +125,16 @@ class ArtCollectionUpdateArtpieces(generics.GenericAPIView):
                 try:
                     artpiece_to_add = Artpiece.objects.get(id=artpiece_id)
                     if artpiece_to_add.owner != request.user:
-                        raise PermissionDenied(detail=f"You do not have permission to add artpiece {artpiece_id}.")
+                        raise PermissionDenied(
+                            detail="You can only add your own artpieces.")
                     artpiece_to_add.add_to_collection(art_collection)
                 except Artpiece.DoesNotExist:
                     # Handle case where artpiece doesn't exist
-                    return Response({'detail': f'Artpiece with ID {artpiece_id} not found.'}, status=status.HTTP_404_NOT_FOUND)
+                    return Response(
+                        {
+                            'detail':
+                            (f'Artpiece with ID {artpiece_id} not found.')
+                        }, status=status.HTTP_404_NOT_FOUND)
 
             # Refresh the art collection instance
             art_collection.refresh_from_db()
@@ -74,8 +144,17 @@ class ArtCollectionUpdateArtpieces(generics.GenericAPIView):
             return Response(serializer.data)
 
         except ArtCollection.DoesNotExist:
-            return Response({'detail': 'Art collection not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {'detail': 'Art collection not found.'},
+                status=status.HTTP_404_NOT_FOUND
+                )
         except Artpiece.DoesNotExist:
-            return Response({'detail': 'One or more artpieces not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {'detail': 'One or more artpieces not found.'},
+                status=status.HTTP_404_NOT_FOUND
+                )
         except PermissionDenied as e:
-            return Response({'detail': str(e)}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {'detail': str(e)},
+                status=status.HTTP_403_FORBIDDEN
+                )
--- viridian_api/permissions.py
+++ viridian_api/permissions.py
@@ -20,7 +20,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 class IsOwner(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
+
+    Methods:
+        has_object_permission(self, request, view, obj):
+            Returns true if the requesting user is the owner of the object,
+            else false.
     """
     def has_object_permission(self, request, view, obj):
-        # Permissions are only allowed to the owner of the object.
         return obj.owner == request.user
