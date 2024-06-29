1848940c38b7fc1fe537a3a0fda76ff19f534111
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-06-29 20:35:15 +0200

    feat: Add tests for ArtCollectionDetail view

--- art_collections/tests.py
+++ art_collections/tests.py
@@ -49,6 +49,56 @@ class ArtCollectionListTests(APITestCase):
         """
         data = {'title': 'New Collection'}
         response = self.client.post(self.url_list, data)
-        print("RESPONSE ", response.content)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(ArtCollection.objects.count(), 1)
+
+
+class ArtCollectionDetailTests(APITestCase):
+    """
+    Test suite for testing ArtCollectionDetail view, endpoint /collections/id/,
+    and specifically CRUD operations on an individual Art Collection.
+    """
+    def setUp(self):
+        """
+        Set up test user and a test Art Collection instance.
+        """
+        self.user = CustomUser.objects.create_user(
+            email='test@test.com',
+            username='testname',
+            password='testpass')
+
+        self.art_collection = ArtCollection.objects.create(
+            owner=self.user,
+            title='Test Collection'
+            )
+        self.url_detail = reverse(
+            'collection-detail',
+            args=[self.art_collection.id])
+
+    def test_can_retrieve_art_collection(self):
+        """
+        Test that an anonymous user can retrieve an art collection by ID.
+        """
+        response = self.client.get(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_update_art_collection(self):
+        """
+        Test that a logged in user can update their art collection.
+        """
+        self.client.login(email='test@test.com', password='testpass')
+        data = {'title': 'Updated Art Collection'}
+        response = self.client.put(self.url_detail, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.art_collection.refresh_from_db()
+        self.assertEqual(self.art_collection.title, 'Updated Art Collection')
+
+    def test_logged_in_user_can_delete_art_collection(self):
+        """
+        Test that a logged in user can delete their art collection.
+        """
+        self.client.login(email='test@test.com', password='testpass')
+        response = self.client.delete(self.url_detail)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        with self.assertRaises(ArtCollection.DoesNotExist):
+            ArtCollection.objects.get(id=self.art_collection.id)
