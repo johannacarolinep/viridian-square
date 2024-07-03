32efc860b9c3fdf6a6e9d596f2c7c5e69da3b72e
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-03 18:24:22 +0200

    feat: Filter queryset in EnquiryList view

--- enquiries/views.py
+++ enquiries/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Q
+from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import EnquirySerializer
 from .models import Enquiry
@@ -7,7 +8,26 @@ from .models import Enquiry
 class EnquiryList(generics.ListCreateAPIView):
     serializer_class = EnquirySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Enquiry.objects.all()
+    queryset = Enquiry.objects.all().order_by('-updated_on')
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = [
+        'buyer',
+        'artpiece__owner'
+    ]
+    ordering_fields = [
+        'updated_on',
+    ]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = super().get_queryset()
+
+        queryset = queryset.filter(Q(buyer=user) | Q(artpiece__owner=user))
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(buyer=self.request.user)
