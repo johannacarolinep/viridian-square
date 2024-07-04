3a3a3dac917ddd6fa3cc44905e86e7a24d0c7211
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-04 16:32:23 +0200

    feat: Make small adjustments to EnquirySerializer, add docstrings

--- enquiries/serializers.py
+++ enquiries/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Enquiry
 from artpieces.models import Artpiece
-from users.models import CustomUser
 
 
 class EnquirySerializer(serializers.ModelSerializer):
@@ -9,30 +8,44 @@ class EnquirySerializer(serializers.ModelSerializer):
     This serializer handles the serialization and deserialization of
     Enquiry objects, including validation of fields.
     """
-    buyer = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), allow_null=True)
+    buyer_name = serializers.SerializerMethodField()
     is_buyer = serializers.SerializerMethodField()
     buyer_profile_id = serializers.SerializerMethodField()
     buyer_profile_image = serializers.SerializerMethodField()
-    artpiece = serializers.PrimaryKeyRelatedField(queryset=Artpiece.objects.all(), allow_null=True)
-    artist = serializers.SerializerMethodField()
+    artpiece = serializers.PrimaryKeyRelatedField(
+        queryset=Artpiece.objects.all(), allow_null=True)
+    artist_name = serializers.SerializerMethodField()
     is_artist = serializers.SerializerMethodField()
     artist_profile_id = serializers.SerializerMethodField()
     artist_profile_image = serializers.SerializerMethodField()
 
+    def get_buyer_name(self, obj):
+        """
+        Returns the name of the buyer from the profile,
+        given the buyer is not null.
+        """
+        if obj.buyer and obj.buyer.profile:
+            return obj.buyer.profile.name
+        return None
+
     def get_is_buyer(self, obj):
         """
-        Returns True if the requesting user is the owner of
-        the collection.
+        Returns True if the requesting user is the buyer.
         """
         request = self.context['request']
         return request.user == obj.buyer
 
     def get_buyer_profile_id(self, obj):
+        """ Returns the buyer's profile id, if the buyer is not null. """
         if obj.buyer is None:
             return None
         return obj.buyer.profile.id
 
     def get_buyer_profile_image(self, obj):
+        """
+        Returns the buyer's profile image url,
+        if the buyer field is not null.
+        """
         if obj.buyer is None:
             return None
         return obj.buyer.profile.profile_image.url
@@ -40,22 +53,34 @@ class EnquirySerializer(serializers.ModelSerializer):
     def get_is_artist(self, obj):
         """
         Returns True if the requesting user is the owner of
-        the collection.
+        the artpiece.
         """
         request = self.context['request']
         return obj.artpiece is not None and request.user == obj.artpiece.owner
 
-    def get_artist(self, obj):
+    def get_artist_name(self, obj):
+        """
+        Returns the artists name from the profile, if the artpiece field is not
+        set to null.
+        """
         if obj.artpiece is None:
             return None
         return obj.artpiece.owner.profile.name
 
     def get_artist_profile_id(self, obj):
+        """
+        Returns the artist's profile id, if the artpiece field
+        is not set to null.
+        """
         if obj.artpiece is None:
             return None
         return obj.artpiece.owner.profile.id
 
     def get_artist_profile_image(self, obj):
+        """
+        Returns the artist's profile image url, if the artpiece field is not
+        set to null.
+        """
         if obj.artpiece is None:
             return None
         return obj.artpiece.owner.profile.profile_image.url
@@ -63,7 +88,7 @@ class EnquirySerializer(serializers.ModelSerializer):
     def validate(self, data):
         """
         Validate an artpiece not belonging to the requesting user is
-        provided on creation.
+        provided on creation, and that the provided artpiece is for sale.
         """
         request = self.context['request']
 
@@ -75,6 +100,10 @@ class EnquirySerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(
                     "Artpiece field is required."
                     )
+            if data.get('artpiece').for_sale != 1:
+                raise serializers.ValidationError(
+                    "This artpiece is not for sale."
+                )
         return data
 
     def validate_initial_message(self, value):
@@ -88,8 +117,8 @@ class EnquirySerializer(serializers.ModelSerializer):
     class Meta:
         model = Enquiry
         fields = [
-            'id', 'buyer', 'is_buyer', 'buyer_profile_id',
-            'buyer_profile_image', 'artpiece', 'artist', 'is_artist',
+            'id', 'buyer_name', 'is_buyer', 'buyer_profile_id',
+            'buyer_profile_image', 'artpiece', 'artist_name', 'is_artist',
             'artist_profile_id', 'artist_profile_image', 'initial_message',
             'response_message', 'created_on', 'updated_on', 'status',
             'buyer_has_checked', 'artist_has_checked',
