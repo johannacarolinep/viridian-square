327a868fca7bc3a0b1a63180214030b819c046d0
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-05 11:35:48 +0200

    feat: Set a default datetime format in settings.py

--- viridian_api/settings.py
+++ viridian_api/settings.py
@@ -34,6 +34,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 8,
+    'DATETIME_FORMAT': '%d %b %Y %H:%M',
 }
 
 if 'DEV' not in os.environ:
