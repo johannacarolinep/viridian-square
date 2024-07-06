67e8ef9c18bb32572162b8b10216e97d0f1e2ca1
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-06 21:42:43 +0200

    feat: Add routing with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import styles from "./App.module.css";
-import NavBar from './components/navbar/NavBar';
-
+import NavBar from "./components/navbar/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container fluid className={styles.Main}>
+        <Routes>
+          <Route exact path="/" element={<h1>Discovery page</h1>} />
+          <Route exact path="/signup" element={<h1>Sign up page</h1>} />
+          <Route exact path="/signin" element={<h1>Sign in page</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
