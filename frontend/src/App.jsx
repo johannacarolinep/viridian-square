5d5a208e153a2232e74a667278f76768b2f30d63
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-07 11:16:11 +0200

    feat: Add a basic sign up form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/navbar/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Routes } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./containers/auth/SignUpForm";
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <Container fluid className={styles.Main}>
         <Routes>
           <Route exact path="/" element={<h1>Discovery page</h1>} />
-          <Route exact path="/signup" element={<h1>Sign up page</h1>} />
+          <Route exact path="/signup" element={<SignUpForm />} />
           <Route exact path="/signin" element={<h1>Sign in page</h1>} />
         </Routes>
       </Container>
