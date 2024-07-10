6032c264a70b98815bc87fd733d1f39f85b80fa8
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-10 11:04:56 +0200

    feat: Add filtering by art medium and for sale status in Discover component

--- frontend/src/components/discover/Discover.jsx
+++ frontend/src/components/discover/Discover.jsx
@@ -3,17 +3,26 @@ import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import { axiosReq } from "../../api/axiosDefaults";
+import { Button } from "react-bootstrap";
 
 const Discover = () => {
   const [artpieces, setArtpieces] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
 
   const [query, setQuery] = useState("");
+  const [filterMedium, setFilterMedium] = useState("");
+  const [filterForSale, setFilterForSale] = useState("");
+
+  const handleToggleForSale = () => {
+    setFilterForSale((prevFilter) => (prevFilter === "" ? "1" : ""));
+  };
 
   useEffect(() => {
     const fetchArtpieces = async () => {
       try {
-        const { data } = await axiosReq.get(`/artpieces/?search=${query}`);
+        const { data } = await axiosReq.get(
+          `/artpieces/?search=${query}&art_medium=${filterMedium}&for_sale=${filterForSale}`
+        );
         setArtpieces(data);
         setHasLoaded(true);
       } catch (err) {
@@ -28,7 +37,7 @@ const Discover = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [query]);
+  }, [query, filterMedium, filterForSale]);
 
   return (
     <>
@@ -40,12 +49,39 @@ const Discover = () => {
           className="mr-sm-2"
           placeholder="Search posts"
         />
+        <Form.Control
+          as="select"
+          value={filterMedium}
+          onChange={(event) => setFilterMedium(event.target.value)}
+        >
+          <option value="">All mediums</option>
+          <option value="oil">Oil</option>
+          <option value="watercolour">Watercolour</option>
+          <option value="gouache">Gouache</option>
+          <option value="acrylic">Acrylic</option>
+          <option value="charcoal">Charcoal</option>
+          <option value="chalk">Chalk</option>
+          <option value="photography">Photography</option>
+          <option value="mixedmedia">Mixed media</option>
+          <option value="other">Other</option>
+        </Form.Control>
+
+        <Button onClick={handleToggleForSale}>
+          {filterForSale === "1" ? "Show all" : "For sale"}
+        </Button>
       </Form>
 
       {hasLoaded ? (
         <>
           {artpieces.results.length ? (
-            artpieces.results.map((artpiece) => <p>{artpiece.title}</p>)
+            artpieces.results.map((artpiece) => (
+              <div>
+                <h3>{artpiece.title}</h3>
+                <p>{artpiece.for_sale}</p>
+                <p>{artpiece.art_medium}</p>
+                <p>{artpiece.hashtags}</p>
+              </div>
+            ))
           ) : (
             <Container>
               <p>No results</p>
