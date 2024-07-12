98b835fec9e5017f28e926b130d437aa27ef3a64
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-12 18:24:21 +0200

    feat: Add hero section to LikedPage

--- frontend/src/containers/LikedPage/LikedPage.jsx
+++ frontend/src/containers/LikedPage/LikedPage.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Discover from "../../components/discover/Discover";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import appStyles from "../../App.module.css";
+import styles from "./LikedPage.module.css";
 
 const LikedPage = () => {
   const currentUser = useCurrentUser();
 
   return (
     <main>
-      <h1>LikedPage</h1>
+      <section className={`${appStyles.bgPrimary} ${styles.HeroSection} fs-1`}>
+        <h1 className={`fw-bold mb-0`}>Your liked artpieces </h1>
+        <i
+          className={`fa-solid fa-heart ${appStyles.txtWhite} ${appStyles.txtLarger}`}
+        ></i>
+      </section>
       <Discover likesFilter={`likes__owner=${currentUser?.pk || ""}&`} />
     </main>
   );
