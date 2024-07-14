21ce325a5be45b7c847345063201dd4f1ae7016f
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-14 22:43:37 +0200

    feat: Add form to update email on AccountPage

--- frontend/src/containers/AccountPage/AccountPage.jsx
+++ frontend/src/containers/AccountPage/AccountPage.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+import { useNavigate } from "react-router-dom";
+import { Button, Container, Form } from "react-bootstrap";
+import appStyles from "../../App.module.css";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const AccountPage = () => {
-  return <main>AccountPage</main>;
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    new_password1: "",
+    new_password2: "",
+    old_password: "",
+  });
+
+  const { email } = userData;
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const handleMount = async () => {
+      if (currentUser) {
+        setUserData({
+          ...userData,
+          email: currentUser.email,
+        });
+      } else {
+        navigate("/signin");
+      }
+    };
+
+    handleMount();
+  }, [currentUser, navigate]);
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleEmailSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("/update-email/", {
+        email,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        email,
+      }));
+    } catch (err) {
+      //   console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <main>
+      <Container fluid="xl">
+        <h1>Account center</h1>
+        <Form onSubmit={handleEmailSubmit}>
+          <h2>Change email address:</h2>
+          <Form.Group>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.email?.map((message, idx) => (
+            <p key={idx}>{message}</p>
+          ))}
+          <Button className={`my-3 me-3 ${appStyles.btnPrimary}`} type="submit">
+            Update
+          </Button>
+        </Form>
+        AccountPage
+      </Container>
+    </main>
+  );
 };
 
 export default AccountPage;
