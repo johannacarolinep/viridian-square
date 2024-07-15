53b50f83102450a630fa68d5af7eb00f762eb567
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-15 11:26:09 +0200

    feat: Add structure and style to account page

--- frontend/src/containers/AccountPage/AccountPage.jsx
+++ frontend/src/containers/AccountPage/AccountPage.jsx
@@ -4,7 +4,7 @@ import {
   useSetCurrentUser,
 } from "../../contexts/CurrentUserContext";
 import { useNavigate } from "react-router-dom";
-import { Button, Container, Form } from "react-bootstrap";
+import { Accordion, Button, Container, Form } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 import { removeTokenTimestamp } from "../../utils/utils";
@@ -16,12 +16,14 @@ const AccountPage = () => {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({
     email: "",
+    current_email: "",
     new_password1: "",
     new_password2: "",
     password: "",
   });
 
-  const { email, new_password1, new_password2, password } = userData;
+  const { email, current_email, new_password1, new_password2, password } =
+    userData;
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
@@ -30,6 +32,7 @@ const AccountPage = () => {
         setUserData({
           ...userData,
           email: currentUser.email,
+          current_email: currentUser.email,
         });
       } else {
         navigate("/signin");
@@ -96,78 +99,111 @@ const AccountPage = () => {
   };
 
   return (
-    <main>
+    <main className={`${appStyles.bgPrimary}`}>
       <Container fluid="xl">
-        <h1>Account center</h1>
-        <Form onSubmit={handleEmailSubmit}>
-          <h2>Change email address:</h2>
-          <Form.Group>
-            <Form.Label>Email:</Form.Label>
-            <Form.Control
-              type="text"
-              name="email"
-              value={email}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          {errors.email?.map((message, idx) => (
-            <p key={idx}>{message}</p>
-          ))}
-          <Button className={`my-3 me-3 ${appStyles.btnPrimary}`} type="submit">
-            Update
-          </Button>
-        </Form>
+        <div className={`${appStyles.bgAccentLight} p-3 my-4 mx-3 rounded`}>
+          <h1>Account center</h1>
+          <p>
+            You are logged in as{" "}
+            <span className="fw-bold">{current_email}</span>
+          </p>
+          <Accordion defaultActiveKey="">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                <h2>Change email address:</h2>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Form onSubmit={handleEmailSubmit}>
+                  <Form.Group>
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.email?.map((message, idx) => (
+                    <p key={idx}>{message}</p>
+                  ))}
+                  <Button
+                    className={`my-3 me-3 ${appStyles.btnPrimary}`}
+                    type="submit"
+                  >
+                    Update
+                  </Button>
+                </Form>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>
+                <h2>Change password:</h2>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Form onSubmit={handlePasswordSubmit}>
+                  <Form.Group>
+                    <Form.Label>New password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="new_password1"
+                      value={new_password1}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.new_password1?.map((message, idx) => (
+                    <p key={idx}>{message}</p>
+                  ))}
 
-        <Form onSubmit={handlePasswordSubmit}>
-          <h2>Change password:</h2>
-          <Form.Group>
-            <Form.Label>New password:</Form.Label>
-            <Form.Control
-              type="password"
-              name="new_password1"
-              value={new_password1}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          {errors.new_password1?.map((message, idx) => (
-            <p key={idx}>{message}</p>
-          ))}
-
-          <Form.Group>
-            <Form.Label>Confirm new password:</Form.Label>
-            <Form.Control
-              type="password"
-              name="new_password2"
-              value={new_password2}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          {errors.new_password2?.map((message, idx) => (
-            <p key={idx}>{message}</p>
-          ))}
-          <Button className={`my-3 me-3 ${appStyles.btnPrimary}`} type="submit">
-            Update password
-          </Button>
-        </Form>
-
-        <Form onSubmit={handleAccountDelete}>
-          <h2>Delete account:</h2>
-          <Form.Group>
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              value={password}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          {errors.password?.map((message, idx) => (
-            <p key={idx}>{message}</p>
-          ))}
-          <Button className={`my-3 me-3 ${appStyles.btnPrimary}`} type="submit">
-            Delete account
-          </Button>
-        </Form>
+                  <Form.Group>
+                    <Form.Label>Confirm new password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="new_password2"
+                      value={new_password2}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.new_password2?.map((message, idx) => (
+                    <p key={idx}>{message}</p>
+                  ))}
+                  <Button
+                    className={`my-3 me-3 ${appStyles.btnPrimary}`}
+                    type="submit"
+                  >
+                    Update password
+                  </Button>
+                </Form>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>
+                <h2>Delete account:</h2>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Form onSubmit={handleAccountDelete}>
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      value={password}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.password?.map((message, idx) => (
+                    <p key={idx}>{message}</p>
+                  ))}
+                  <Button
+                    className={`my-3 me-3 ${appStyles.btnPrimary}`}
+                    type="submit"
+                  >
+                    Delete account
+                  </Button>
+                </Form>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
       </Container>
     </main>
   );
