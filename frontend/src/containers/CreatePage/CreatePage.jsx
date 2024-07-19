6180cd13b92ddbb5532c1ed8bd2a89e166b1fbe0
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-19 15:41:28 +0200

    feat: Add structure and style to create page

--- frontend/src/containers/CreatePage/CreatePage.jsx
+++ frontend/src/containers/CreatePage/CreatePage.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useRedirect } from "../../hooks/useRedirect";
+import { Card, Col, Container, Image, Row } from "react-bootstrap";
+import Frame from "../../assets/images/frame.webp";
+import Collection from "../../assets/images/collection.webp";
+import Asset from "../../components/asset/Asset";
+import appStyles from "../../App.module.css";
+import styles from "./CreatePage.module.css";
 
 const CreatePage = () => {
   useRedirect("loggedOut", "/");
 
   return (
-    <main>
-      <Link to="/create-collection">New collection</Link>
-      <Link to="/create-artpiece">New artpiece</Link>
+    <main className={`d-flex align-items-center ${appStyles.bgWhite}`}>
+      <Container fluid="xl">
+        <Row className={`g-3 g-md-0 m-0 my-2 my-md-4`}>
+          <Col xs={12} md={6} className={`h-100 d-flex justify-content-center`}>
+            <Link to="/create-artpiece" className={styles.LinkCard}>
+              <div className={`${styles.ImgContainer}`}>
+                <Image src={Frame} />
+              </div>
+              <div className="fs-3 mt-3 fw-bold">New artpiece</div>
+            </Link>
+          </Col>
+          <Col xs={12} md={6} className={`h-100 d-flex justify-content-center`}>
+            <Link to="/create-collection" className={styles.LinkCard}>
+              <div className={`${styles.ImgContainer}`}>
+                <Image src={Collection} />
+              </div>
+              <div className="fs-3 mt-3 fw-bold">New collection</div>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </main>
   );
 };
