6651528dc0ac0cb103d73488efe4472f2d7ded67
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-07-19 11:37:52 +0200

    feat: Add structure and styles to sign in and sign up

--- frontend/src/containers/SignInPage/SignInPage.jsx
+++ frontend/src/containers/SignInPage/SignInPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, Image } from "react-bootstrap";
 import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { setTokenTimestamp } from "../../utils/utils";
 import { useRedirect } from "../../hooks/useRedirect";
+import appStyles from "../../App.module.css";
+import styles from "./SignInPage.module.css";
+import SignIn from "../../assets/images/signin.webp";
 
 const SignInForm = () => {
   useRedirect("loggedIn", "/");
@@ -40,42 +42,69 @@ const SignInForm = () => {
     }
   };
   return (
-    <main>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        {errors.email?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
+    <main className="d-flex align-items-center">
+      <Container fluid="xl" className="p-0 p-md-4 p-lg-5">
+        <Form onSubmit={handleSubmit} className="my-md-3">
+          <Row className={`${appStyles.bgWhite} p-4 m-0`}>
+            <Col lg={6} className="h-100">
+              <h1>Sign in:</h1>
+              <div className={`${appStyles.dividerPrimary} mb-3`}></div>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.email?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
 
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        {errors.password?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
-        <Button variant="primary" type="submit">
-          Sign In
-        </Button>
-        {errors.non_field_errors?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
-      </Form>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
+
+              <Button
+                className={`my-3 me-3`}
+                variant="secondary"
+                onClick={() => navigate("/")}
+              >
+                Cancel
+              </Button>
+              <Button
+                className={`my-3 me-3 ${appStyles.btnPrimary}`}
+                type="submit"
+              >
+                Sign In
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
+            </Col>
+            <Col
+              lg={6}
+              className={`d-flex align-items-center justify-content-center`}
+            >
+              <div className={`${styles.ImgContainer}`}>
+                <Image src={SignIn} />
+              </div>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
     </main>
   );
 };
--- frontend/src/containers/SignUpPage/SignUpPage.jsx
+++ frontend/src/containers/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, Image } from "react-bootstrap";
 import axios from "axios";
 import { useRedirect } from "../../hooks/useRedirect";
+import appStyles from "../../App.module.css";
+import styles from "./SignUpPage.module.css";
+import Napoleon from "../../assets/images/napoleon.webp";
 
 const SignUpForm = () => {
   useRedirect("loggedIn", "/");
@@ -39,59 +42,86 @@ const SignUpForm = () => {
     }
   };
   return (
-    <main>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else without your express
-            permission.
-          </Form.Text>
-        </Form.Group>
-        {errors.email?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
+    <main className="d-flex align-items-center">
+      <Container fluid="xl" className="p-0 p-md-4 p-lg-5">
+        <Form onSubmit={handleSubmit} className="my-md-3">
+          <Row className={`${appStyles.bgWhite} p-4 m-0`}>
+            <Col lg={6} className="h-100">
+              <h1>Sign up:</h1>
+              <div className={`${appStyles.dividerPrimary} mb-3`}></div>
+              <Form.Group controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else without your
+                  express permission.
+                </Form.Text>
+              </Form.Group>
+              {errors.email?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
 
-        <Form.Group className="mb-3" controlId="password1">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password1"
-            value={password1}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        {errors.password1?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
-        <Form.Group className="mb-3" controlId="password2">
-          <Form.Label>Password again</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password2"
-            value={password2}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        {errors.password2?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
-        <Button variant="primary" type="submit">
-          Sign Up
-        </Button>
-        {errors.non_field_errors?.map((message, idx) => (
-          <p key={idx}>{message}</p>
-        ))}
-      </Form>
+              <Form.Group className="mb-3" controlId="password1">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password1"
+                  value={password1}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password1?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
+              <Form.Group className="mb-3" controlId="password2">
+                <Form.Label>Password again</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password2"
+                  value={password2}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password2?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
+
+              <Button
+                className={`my-3 me-3`}
+                variant="secondary"
+                onClick={() => navigate("/")}
+              >
+                Cancel
+              </Button>
+              <Button
+                className={`my-3 me-3 ${appStyles.btnPrimary}`}
+                type="submit"
+              >
+                Sign Up
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <p key={idx}>{message}</p>
+              ))}
+            </Col>
+            <Col
+              lg={6}
+              className={`d-flex align-items-center justify-content-center`}
+            >
+              <div className={`${styles.ImgContainer}`}>
+                <Image src={Napoleon} />
+              </div>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
     </main>
   );
 };
